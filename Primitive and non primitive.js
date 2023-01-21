d33147e39ccef9f8409fa444c34b3530fa2e6ef6
Pawan <pawanchaukiyal@gmail.com>
2023-01-21 13:43:16 +0530

    Second day

---
+++ Primitive and non primitive.js
@@ -0,0 +1,76 @@
+console.log("Let see Primitive and Non Primitive Data types");
+
+/*
+// let See Primitive DataTypes
+
+console.log("Primitive Datatypes : ");
+console.log("Primitive datatypes are 7 types : ");
+console.log("NULL, Number, String , Symbol, Undefined, Boolean, BigInt");
+
+// null
+let a=null;
+console.log(typeof a);  // type --- object
+// number 
+let b=654;
+console.log(typeof b); // type --- number
+// string
+let c="Pawan";
+console.log(typeof c); // type --- string
+// symbol
+let d=Symbol("This is a symbol ");
+console.log(typeof d); // type --- symbol
+// Bigint
+let e=BigInt("564")-BigInt(100);
+console.log(typeof e); // type --- bigint
+// Boolean
+let f="false"
+console.log(typeof f); // type --- string
+
+// 
+console.log(a, b, c, d, e, f);
+
+*/
+
+/*
+// Non primitive datatype
+console.log(" This is a non primitive type ");
+console.log("And Object is a non prmitive datatype ");
+
+// let see:
+
+// let items = {
+//   Name : " Mouse",
+//   color : " Black",
+//   Price : 564
+// };
+// console.log(items); 
+// console.log(items.Name);
+// console.log(typeof items.Name);
+// console.log(typeof items.Price);
+
+// let do some experiment
+
+// //1 here, i change name and price
+//  let items = {
+// //   Name : Mouse,
+//   color : " Black",
+//   Price : "564"
+// };
+// console.log(items); 
+// // console.log(items.Name);  // error: Mouse is not defined
+// console.log(typeof items.Name); // undefined
+// console.log(typeof items.Price); // string
+
+
+// //2 put quotes on keys
+// let items = {
+//     "Name" : " Mouse",
+//     "color" : " Black",
+//     "Price" : 564
+//   };
+//   console.log(items); 
+//   console.log(items.Name);
+//   console.log(typeof items.Name); // string 
+//   console.log(typeof items.Price); // number
+
+*/
