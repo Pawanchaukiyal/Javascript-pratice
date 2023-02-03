e93230b6ceebe69ce69fae3167bd5088ac9615a2
Pawan <pawanchaukiyal@gmail.com>
2023-02-03 18:48:14 +0530

    day 13

---
+++ Dom/1_Methods.js
@@ -0,0 +1,17 @@
+console.log("This is a HTML DOM method");
+
+// HTML DOM methods are action that you can perfom on HTML elements
+
+// And HTML DOM properties are values (of HTML elements) that you can change or set 
+
+console.log("I can add the paragraph in p_ id: 1_Demo ");
+console.log(" using getElementById and innnerHTML in java script.")
+
+// let do that
+
+//document.getElementById("1_Demo").innerHTML = "Hello World!";
+ document.getElementById("1_Demo").innerHTML="I add this by getElementById() method and innerHTML property";
+
+ // getElementById(arguments), is a method
+
+ // innerHTML, is a property
